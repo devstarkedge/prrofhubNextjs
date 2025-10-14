c2be7b47b508bd865d6296278a4cf364d6c05584
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2025-10-14 17:16:25 +0530

    due tasks list added

--- app/utils/apiUtils.js
+++ app/utils/apiUtils.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 import { API_BASE_URL, API_KEYS } from "./constants";
+import { getFormattedDate } from "./dateUtils";
 
-export const fetchTimeEntries = async (
-  employeeId,
-  from,
-  to
-) => {
+export const fetchTimeEntries = async (employeeId, from, to) => {
   const apiKey = API_KEYS[employeeId];
   if (!apiKey) return [];
-  const response = await axios.get(`${API_BASE_URL}/alltime?from_date=${from}&to_date=${to}`, {
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-KEY": apiKey,
-    },
-  });
+  const response = await axios.get(
+    `${API_BASE_URL}/alltime?from_date=${from}&to_date=${to}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": apiKey,
+      },
+    }
+  );
   const data = response.data || [];
   console.log(data);
   return data.filter((entry) => entry.by_me === true);
@@ -33,6 +33,7 @@ export const fetchTodoCount = async (id) => {
 
 export const fetchTodos = async (id, from, to) => {
   const apiKey = API_KEYS[id];
+  console.log(id);
   if (!apiKey) return [];
   const response = await axios.get(`${API_BASE_URL}/alltodo`, {
     headers: {
@@ -42,18 +43,24 @@ export const fetchTodos = async (id, from, to) => {
   });
   const data = response.data || [];
 
-  console.log(data)
- 
+  console.log("todo",data);
+
+  // Filter by assigned
+  const assignedFiltered = data.filter((todo) => {
+    return todo.assigned && todo.assigned.includes(id);
+  });
+
   if (from && to) {
     const fromDate = new Date(from);
     const toDate = new Date(to);
-    return data.filter((todo) => {
-      if (todo.created_at) {
-        const todoDate = new Date(todo.created_at);
+    return assignedFiltered.filter((todo) => {
+      if (todo.due_date) {
+        const todoDate = new Date(todo.due_date);
         return todoDate >= fromDate && todoDate <= toDate;
       }
-      return true; 
+      return false; // If no due_date, exclude from date range filters
     });
   }
-  return data;
+
+  return assignedFiltered;
 };
